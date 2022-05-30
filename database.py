b4c42e5877a8ca8ad7a527b96b75bdd79eeabca4
Song Mingwei <songmingwei1998@gmail.com>
2022-05-30 23:42:06 +0800

    fix not committing transactions

--- database.py
+++ database.py
@@ -5,16 +5,16 @@ import config
 def execute(command: str):
     
     data = None
-    connection = psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require')
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(command)
-            try:
-                data = cursor.fetchall()
-            except psycopg2.ProgrammingError as error:
-                pass
-            except Exception as error:
-                print(error)
+        with psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(command)
+                try:
+                    data = cursor.fetchall()
+                except psycopg2.ProgrammingError as error:
+                    pass
+                except Exception as error:
+                    print(error)
     except Exception as error:
         print(error)
     finally:
@@ -25,20 +25,20 @@ def execute(command: str):
 # Create table of users if it does not exist
 def create_if_not_exists() -> None:
     
-    connection = psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require')
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                CREATE TABLE IF NOT EXISTS users (
-                    user_id        INTEGER PRIMARY KEY,
-                    rank_and_name  TEXT NOT NULL,
-                    company        TEXT NOT NULL,
-                    username       TEXT NOT NULL,
-                    UNIQUE (rank_and_name, company)
-                );
-                """
-            )
+        with psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    CREATE TABLE IF NOT EXISTS users (
+                        user_id        INTEGER PRIMARY KEY,
+                        rank_and_name  TEXT NOT NULL,
+                        company        TEXT NOT NULL,
+                        username       TEXT NOT NULL,
+                        UNIQUE (rank_and_name, company)
+                    );
+                    """
+                )
     except Exception as error:
         print(f'Could not create users db: {error}')
     finally:
@@ -49,18 +49,18 @@ def create_if_not_exists() -> None:
 # Create a new user or replace an existing one  
 def add_user(user_id: int, user_data: dict):
     
-    connection = psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require')
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                INSERT INTO users (user_id, rank_and_name, company, username)
-                VALUES (%s, %s, %s, %s)
-                ON CONFLICT (user_id)
-                DO UPDATE SET (rank_and_name, company, username) = (EXCLUDED.rank_and_name, EXCLUDED.company, EXCLUDED.username);
-                """,
+        with psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    INSERT INTO users (user_id, rank_and_name, company, username)
+                    VALUES (%s, %s, %s, %s)
+                    ON CONFLICT (user_id)
+                    DO UPDATE SET (rank_and_name, company, username) = (EXCLUDED.rank_and_name, EXCLUDED.company, EXCLUDED.username);
+                    """,
                 (user_id, user_data['rank_and_name'], user_data['company'], user_data['username'])
-            )
+                )
     except Exception as error:
         print(f'Could not add user to db: {error}')
     finally:
@@ -72,18 +72,18 @@ def add_user(user_id: int, user_data: dict):
 def retrieve_user(user_id: int):
     
     user_data = None
-    connection = psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require')
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                SELECT rank_and_name, company, username
-                FROM users
-                WHERE user_id = %s
-                """,
-                (user_id,) # (user_id) is an int while (user_id,) is a tuple containing an int
-            )
-            user_data = cursor.fetchone()
+        with psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT rank_and_name, company, username
+                    FROM users
+                    WHERE user_id = %s
+                    """,
+                    (user_id,) # (user_id) is an int while (user_id,) is a tuple containing an int
+                )
+                user_data = cursor.fetchone()
     except Exception as error:
         print(f'Could not retrieve user from db: {error}')
     finally:
@@ -102,19 +102,19 @@ def retrieve_user(user_id: int):
 def retrieve_user_by_rank_name_company(rank_and_name: str, company: str) -> list:
     
     user_data = None
-    connection = psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require')
     try:
-        with connection.cursor() as cursor:
-            cursor.execute(
-                """
-                SELECT user_id, username
-                FROM users
-                WHERE rank_and_name = %s
-                AND company = %s
-                """,
-                (rank_and_name, company)
-            )
-            user_data = cursor.fetchone()
+        with psycopg2.connect(config.USER_DATABASE_URL, sslmode = 'require') as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT user_id, username
+                    FROM users
+                    WHERE rank_and_name = %s
+                    AND company = %s
+                    """,
+                    (rank_and_name, company)
+                )
+                user_data = cursor.fetchone()
     except Exception as error:
         print(f'Could not retrieve user from db: {error}')
     finally:
