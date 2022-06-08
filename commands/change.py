6e93b13c59cd3f49489bcb58e87d8a1ff05e01a8
Song Mingwei <songmingwei1998@gmail.com>
2022-06-08 13:03:04 +0800

    correct order of params in calendar.delete_booking()

--- commands/change.py
+++ commands/change.py
@@ -457,7 +457,7 @@ def confirm_change(update: Update, context: CallbackContext) -> int:
 def confirm_delete(update: Update, context: CallbackContext) -> int:
     
     try:
-        calendar.delete_booking(context.chat_data, context.user_data)
+        calendar.delete_booking(context.user_data, context.chat_data)
     except Exception as error:
         update.effective_chat.send_message(
             text = '⚠ Sorry, I could not connect to Google Calendar. Try again?',
