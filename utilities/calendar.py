fa56a427014bb8863011a5cb1784f3ebdb05dc85
Song Mingwei <songmingwei1998@gmail.com>
2022-06-08 12:33:04 +0800

    update facilities log channel after changing / creating a booking

--- utilities/calendar.py
+++ utilities/calendar.py
@@ -3,6 +3,7 @@ import logging, json, sys
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+from utilities import actions
 import config
 
 logger = logging.getLogger(__name__)
@@ -228,7 +229,17 @@ def add_booking(user_id: int, user_data: dict, chat_data: dict) -> str:
         }
     ).execute()
     
-    return new_booking.get('htmlLink')
+    actions.update_facilities_channel(
+        "<b>New Booking</b>\n\n"
+        f"<b>Facility:</b> {context.chat_data['facility']}\n"
+        f"<b>Date:</b> {context.chat_data['date']}\n"
+        f"<b>Time:</b> {context.chat_data['start_time']} - {context.chat_data['end_time']}\n"
+        f"<b>Description:</b> {context.chat_data['description']}\n"
+        f"<b>POC:</b> {context.user_data['rank_and_name']} ({context.user_data['company']})"
+        f'<a href="{new_booking["htmlLink"]}">Event Link</a>'
+    )
+    
+    return new_booking['htmlLink']
 
 
 def patch_booking(user_id: int, user_data: dict, chat_data: dict) -> str:
@@ -265,12 +276,33 @@ def patch_booking(user_id: int, user_data: dict, chat_data: dict) -> str:
         }
     ).execute()
     
-    return patched_booking.get('htmlLink')
+    actions.update_facilities_channel(
+        "<b>Booking Updated</b>\n\n"
+        f"<b>Facility:</b> {context.chat_data['old_facility']}{context.chat_data['facility']}\n"
+        f"<b>Date:</b> {context.chat_data['old_date']}{context.chat_data['date']}\n"
+        f"<b>Time:</b> {context.chat_data['old_start_time']}{context.chat_data['old_end_time']}{context.chat_data['start_time']} - {context.chat_data['end_time']}\n"
+        f"<b>Description:</b> {context.chat_data['old_description']}{context.chat_data['description']}\n"
+        f"<b>POC:</b> {context.user_data['rank_and_name']} ({context.user_data['company']})"
+        f'<a href="{patched_booking["htmlLink"]}">Event Link</a>'
+    )
+    
+    return patched_booking['htmlLink']
     
 
-def delete_booking(event_id: str):
+def delete_booking(event_id: str) -> None:
     
-    return service.events().delete(
+    service.events().delete(
         calendarId = config.CALENDAR_ID, 
         eventId = event_id
     ).execute()
+    
+    actions.update_facilities_channel(
+        "<b>Booking Cancelled</b>\n\n"
+        f"<b>Facility:</b> {context.chat_data['facility']}\n"
+        f"<b>Date:</b> {context.chat_data['date']}\n"
+        f"<b>Time:</b> {context.chat_data['start_time']} - {context.chat_data['end_time']}\n"
+        f"<b>Description:</b> {context.chat_data['description']}\n"
+        f"<b>POC:</b> {context.user_data['rank_and_name']} ({context.user_data['company']})"
+    )
+    
+    return
