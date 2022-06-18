43192c48e582862eff64702a9adfab1f4e554c51
Song Mingwei <songmingwei1998@gmail.com>
2022-06-18 17:59:25 +0800

    generate help message from JSON serialised lists of bot commands

--- commands/help.py
+++ commands/help.py
@@ -7,9 +7,19 @@ HELP CALLBACK FUNCTION
 '''
 def show_help(update: Update, context: CallbackContext) -> None:
     
+    # Construct help message
+    message = "Interact with me using these commands:\n\n*Manage Facility Bookings*\n"
+    for command in config.COMMANDS_BOOKING:
+        message += f"/{command[0]} - {command[1]}\n"
+    message += "\n*Bot Settings*\n"
+    for command in config.COMMANDS_SETTING:
+        message += f"/{command[0]} - {command[1]}\n"
+    message += f"\nFor technical assistance, please [contact](tg://user?id={config.ADMIN_UID_LIST[0]}) S3 branch."
+    
+    # Send help message
     context.bot.send_message(
         chat_id = update.effective_chat.id,
-        text = f"{config.BOT_COMMANDS}\n\nIf you experience technical issues, please contact S3 branch.",
+        text = message,
         parse_mode = ParseMode.MARKDOWN
     )
     
