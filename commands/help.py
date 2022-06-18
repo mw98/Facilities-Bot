c665e8f55609d3e300d709f8a6dd58f0e6c9fc5e
Song Mingwei <songmingwei1998@gmail.com>
2022-06-18 18:14:56 +0800

    move construction of commands list to utilities/actions.py

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,6 @@
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler
+from utilities import actions
 import config
 
 '''
@@ -8,18 +9,14 @@ HELP CALLBACK FUNCTION
 def show_help(update: Update, context: CallbackContext) -> None:
     
     # Construct help message
-    message = "Interact with me using these commands:\n\n*Manage Facility Bookings*\n"
-    for command in config.COMMANDS_BOOKING:
-        message += f"/{command[0]} - {command[1]}\n"
-    message += "\n*Bot Settings*\n"
-    for command in config.COMMANDS_SETTING:
-        message += f"/{command[0]} - {command[1]}\n"
-    message += f"\nFor technical assistance, please [contact](tg://user?id={config.ADMIN_UID_LIST[0]}) S3 branch."
-    
+    commands = actions.construct_commands_list()
+        
     # Send help message
     context.bot.send_message(
         chat_id = update.effective_chat.id,
-        text = message,
+        text = 
+            f'{actions.construct_commands_list()}\n\n'
+            f'For technical assistance, please [contact](tg://user?id={config.ADMIN_UID_LIST[0]}) S3 branch.',
         parse_mode = ParseMode.MARKDOWN
     )
     
