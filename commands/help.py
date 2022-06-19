4ba1c813a569b65f1ae86bfd4109471e9d3891ee
Song Mingwei <songmingwei1998@gmail.com>
2022-06-20 00:26:54 +0800

    change config.ADMIN_UID_LIST to ADMIN_USERS

--- commands/help.py
+++ commands/help.py
@@ -16,7 +16,7 @@ def show_help(update: Update, context: CallbackContext) -> None:
         chat_id = update.effective_chat.id,
         text = 
             f'{shared.construct_commands_list()}\n\n'
-            f'For technical assistance, please [contact](tg://user?id={config.ADMIN_UID_LIST[0]}) S3 branch.',
+            f'For technical assistance, please [contact](tg://user?id={config.ADMIN_USERS[0]}) S3 branch.',
         parse_mode = ParseMode.MARKDOWN
     )
     
