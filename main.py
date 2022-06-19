6004ceb8ca2eef2ea90180c5c3066b805d9a5e3d
Song Mingwei <songmingwei1998@gmail.com>
2022-06-20 00:27:48 +0800

    remove admin commands for removed admins

--- main.py
+++ main.py
@@ -35,18 +35,22 @@ def main():
     dispatcher.add_handler(mybookings.handler, 4)
     dispatcher.add_handler(help.handler, 5)
     dispatcher.add_handler(start.handler, 6)
-    
-    # Add bot commands
+        
+    # Configure bot commands
     default_commands = config.COMMANDS_BOOKING + config.COMMANDS_SETTING
     admin_commands = config.COMMANDS_ADMIN + default_commands
     bot.set_my_commands(default_commands)
-    for admin_uid in config.ADMIN_UID_LIST:
-        bot.set_my_commands(
-            commands = admin_commands,
-            scope = BotCommandScopeChat(admin_uid)
-        )
+    for admin_uid in config.ADMIN_USERS:
+        bot.set_my_commands(admin_commands, scope = BotCommandScopeChat(admin_uid))
     
-    # Set default bot admin rights for facilities log channel
+    # Update admin users
+    db_admin_users = database.retrieve_admins()
+    for user_id in db_admin_users - config.ADMIN_USERS:
+        bot.delete_my_commands(BotCommandScopeChat(user_id))
+    for user_id in db_admin_users ^ config.ADMIN_USERS:
+        database.toggle_admin(int(user_id))
+    
+    # Configure default bot admin rights for facilities log channel
     rights = ChatAdministratorRights(
         is_anonymous = False,
         can_manage_chat = False,
@@ -58,7 +62,7 @@ def main():
         can_invite_users = False,
         can_post_messages = True
     )
-    bot.set_my_default_administrator_rights(rights=rights, for_channels=True)
+    bot.set_my_default_administrator_rights(rights, for_channels = True)
 
     # Run bot
     updater.start_webhook(
