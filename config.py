cb2cb0a94a8762c396e18c3915fa132da47e6cea
Song Mingwei <songmingwei1998@gmail.com>
2022-06-28 22:43:15 +0800

    replace FACILITIES_LIST and COMPANIES_LIST with env_vars; remove EVENT_COLOUR_CODES dict

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ HEROKU_APP_URL = os.environ['HEROKU_APP_URL']
 DATABASE_URL = os.environ['DATABASE_URL']
 
 # TELEGRAM
-ADMIN_USERS = set(ujson.loads(os.getenv('ADMIN_USERS') or '[]'))
 BOT_TOKEN = os.environ['BOT_TOKEN']
+ADMIN_USERS = set(ujson.loads(os.getenv('ADMIN_USERS') or '[]'))
 CHANNEL_USERNAME = os.getenv('CHANNEL_USERNAME')
 CHANNEL_MUTED = (os.getenv('CHANNEL_MUTED') == 'True')
 COMMANDS_BOOKING = list(ujson.loads(os.getenv('COMMANDS_BOOKING') or '{}').items())
@@ -19,11 +19,10 @@ WEBHOOK_PORT = os.getenv('PORT') or 8443
 # GOOGLE CALENDAR
 CALENDAR_ID = os.environ['CALENDAR_ID']
 CALENDAR_URL = os.getenv('CALENDAR_URL')
-EVENT_COLOUR_CODES = {'LT 1': '1', 'LT 2': '7', 'CONF ROOM': '2', 'RTS': '5', 'STINGRAY SQ': '6'}
 SERVICE_ACCOUNT_INFO = ujson.loads(os.environ['SERVICE_ACCOUNT_INFO'])
 
-# SHARED VARIABLES
+# BOOKING PARAMETERS
+COMPANIES = ujson.loads(os.environ['COMPANIES'])
+FACILITIES = ujson.loads(os.os.environ['FACILITIES'])
 ALT_FACILITIES = ujson.loads(os.getenv('ALT_FACILITIES') or '{}')
-COMPANIES_LIST = ['ALPHA', 'BRAVO', 'CHARLIE', 'SP', 'MSC', 'HQ']
-FACILITIES_LIST = ['LT 1', 'LT 2', 'CONF ROOM', 'RTS', 'STINGRAY SQ']
 TIMEZONE = timezone(timedelta(hours=8)) # UTC+8
